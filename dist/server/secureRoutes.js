86a64065e67b24b5dbb0c6020452f2bcafb8bb0d
David <david123rana@gmail.com>
2023-10-31 15:49:46 +0530

    ff

--- dist/server/secureRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { checkAuth } from '../middleware/check-auth.js';
-const router = express.Router();
-router.get("/secure-data", checkAuth, (req, res) => {
-  res.status(200).json({
-    message: 'This is secure data!'
-  });
-});
-export default router;
